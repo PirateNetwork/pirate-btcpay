7ad02146885871b631cd7a266cdccca1f62b515e
Hanh <hanh425@gmail.com>
2022-06-15 17:31:10 +0800

    GRPC prototypes

---
+++ walletd/build.rs
@@ -0,0 +1,12 @@
+fn main() {
+    tonic_build::configure()
+        .out_dir("src/generated")
+        .compile(
+            &[
+                "proto/service.proto",
+                "proto/compact_formats.proto",
+            ],
+            &["proto"],
+        )
+        .unwrap();
+}
